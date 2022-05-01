c319155d1880215ed2678f0082caacfbcdbceb32
is_hleb <hlebIshenko@gmail.com>
2022-05-01 21:36:48 +0300

    Test

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/', \App\Http\Controllers\Api\ActionController::class);
-Route::get('/', function (){ echo "Привет"; });
+Route::get('/', function (){ echo "Привет, ДУРАК"; });
