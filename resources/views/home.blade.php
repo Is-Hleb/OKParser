8b52c8d0fc148a28974cd9e739a50b77b2170327
is_hleb <hlebIshenko@gmail.com>
2022-05-03 11:03:56 +0300

    Little fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,34 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <table class="table table-bordered">
-        <thead class="thead-dark">
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">status</th>
-                <th scope="col">output</th>
-                <th scope="col">exception</th>
-                <th scope="col">action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($jobs as $job)
+    <div class="container">
+        <table class="table table-bordered">
+            <thead class="thead-dark">
                 <tr>
-                    <th scope="row" class="fw-bold d-flex justify-content-center">{{ $job->id }}</th>
-                    <th>{{ $job->status }}</th>
-                    <th class="overflow-auto"">
-                        @php(dump($job->output))
-                    </th>
-                    <th>
-                        @php(dump($job->exception))
-                    </th>
-                    <th class="d-flex justify-content-center">
-                        <a href="{{ route('job.delete', $job->id) }}" class="btn btn-danger my-auto">Delete</a>
-                    </th>
+                    <th scope="col">#</th>
+                    <th scope="col">status</th>
+                    <th scope="col">output</th>
+                    <th scope="col">exception</th>
+                    <th scope="col">action</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+            </thead>
+            <tbody>
+                @foreach ($jobs as $job)
+                    <tr>
+                        <th scope="row" class="fw-bold d-flex justify-content-center">{{ $job->id }}</th>
+                        <th>{{ $job->status }}</th>
+                        <th class="overflow-auto"">
+                            @php(dump($job->output))
+                        </th>
+                        <th>
+                            @php(dump($job->exception))
+                        </th>
+                        <th class="d-flex justify-content-center">
+                            <a 
+                                href="{{ route('job.delete', $job->id) }}" 
+                                class="btn btn-danger my-auto"
+                            >
+                                Delete
+                            </a>
+                        </th>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Models\JobInfo;
 */
 Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
-Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+Route::post('logout', [LoginController::class, 'logout'])->name('logout')->middleware('auth');
 
 Route::group(['middleware' => 'auth'], function(){
     Route::get('/', IndexController::class)->name('home');
