7a42b660eecc37f9ca73d2c604176902fa299da1
is_hleb <hlebIshenko@gmail.com>
2022-05-26 10:35:29 +0300

    Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,8 @@ namespace App\Console;
 use App\Models\BotTask;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Models\JobInfo;
+use App\Jobs\OkParserApi;
 
 class Kernel extends ConsoleKernel
 {
@@ -16,11 +18,32 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call(function() {
+        $schedule->call(function () {
             $tasks = BotTask::getQeuueTasks();
-            foreach($tasks as $task) {
+            foreach ($tasks as $task) {
                 $request = $task->dataT;
-                dump($request);
+                $request = json_decode($request, JSON_OBJECT_AS_ARRAY);
+
+                $data = array_filter($request, function ($key) {
+                    return $key !== 'job' && $key != 'action';
+                }, ARRAY_FILTER_USE_KEY);
+
+                $jobInfo = new JobInfo([
+                    'status' => JobInfo::WAITING
+                ]);
+                $jobInfo->save();
+
+                dispatch((new OkParserApi($request['action'], $data, $jobInfo)));
+    
+                $jobInfo = JobInfo::find($jobInfo->id);
+
+                $task->status_task = $jobInfo->status;
+                info("Set answer");
+                $task->answer = json_encode([
+                    'job' => 'get',
+                    'id' => $jobInfo->id
+                ]);
+                $task->save();
             }
         })->everyMinute();
     }
