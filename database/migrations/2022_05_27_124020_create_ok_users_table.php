ac629a26685a376be6e597c953d69a33130c4676
is_hleb <hlebIshenko@gmail.com>
2022-05-30 21:50:58 +0300

    AddUsers

--- database/migrations/2022_05_27_124020_create_ok_users_table.php
+++ database/migrations/2022_05_27_124020_create_ok_users_table.php
@@ -21,7 +21,37 @@ class CreateOkUsersTable extends Migration
             '79171447430:6MPqZR5SQJXPY',
             '79171447581:STcB0dzAK',
             '79266547476:ZqMyKWsx8nex6',
-            '79171420690:papXhYHCx2hjM'
+            '79171420690:papXhYHCx2hjM',
+            '6283198636292:QZ0cEv',
+            '6283157156557:qZ9OpD',
+            '6283177387664:SIc8LP',
+            '6283199094220:W4dohY',
+            '6285947487401:6Dmxhb',
+            '6283159418679:eL0ATr',
+            '6283874886555:NJ5vGo',
+            '6283899039793:qHV5ic',
+            '6283157737473:5Nvu2T',
+            '6283899177543:Pj4s4q',
+            '6283182108387:p3Deki',
+            '6283899483567:1IbmGd',
+            '6283874889537:Tn62o0',
+            '6283899176334:pO4OAl',
+            '6283167732030:1HeVC8',
+            '6283198635283:2gyMls',
+            '6283899488800:7f9J2z',
+            '6287738865062:cA5wZa',
+            '6283899174541:4VxUnT',
+            '6283899172461:Em0O5L',
+            '6283199098329:nc6BqH',
+            '6283124607006:1Kj12f',
+            '6287816954545:B0MqQx',
+            '6283121364857:Iy0mNR',
+            '6283899037278:nT9fC6',
+            '6283167733399:aCz921',
+            '6283123885960:fb8Usq',
+            '6283199094204:NcD1Xq',
+            '6283899413293:bCk7cT',
+            '6283876171010:krP8qm'
         ];
 
         Schema::create('ok_users', function (Blueprint $table) {
@@ -31,7 +61,7 @@ class CreateOkUsersTable extends Migration
             $table->json('cookies')->nullable();
         });
 
-        foreach($users as $user) {
+        foreach ($users as $user) {
             $credits = explode(':', $user);
             OkUser::create([
                 'login' => $credits[0],
