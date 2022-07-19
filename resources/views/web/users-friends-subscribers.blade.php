9148922835ba186e2d72d71f9c3679fc55927774
is_hleb <hlebIshenko@gmail.com>
2022-07-19 16:24:01 +0300

    Add relation

--- resources/views/web/users-friends-subscribers.blade.php
+++ resources/views/web/users-friends-subscribers.blade.php
@@ -29,7 +29,6 @@
                                 $tasks = \App\Models\Task::where('task_id', "node_{$info['task_id']}")->get();
                                 $friendsTsk = $tasks->filter(fn($item, $key) => $item->type == 3)->first();
                                 $subscribersTsk = $tasks->filter(fn($item, $key) => $item->type == 1)->first();
-                                dd($friendsTsk, $subscribersTsk);
                             ?>
                             @if(!$friendsTsk)
                                 <th class="btn-group">
